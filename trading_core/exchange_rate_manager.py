de5ff9b1158a13612d199f23d7a1d63c8bf798b7
Nikita Lesovoy <niklisdon@yandex.ru>
2025-03-12 13:55:06 +0300

    Добавлены модуль курса ядра торговли

---
+++ trading_core/exchange_rate_manager.py
@@ -0,0 +1,12 @@
+import requests
+
+class ExchangeRateManager:
+    @staticmethod
+    def get_cbr_rates():
+        response = requests.get("https://api.cbr.ru/currency/rates")
+        return response.json()
+
+    @staticmethod
+    def get_coingecko_rates():
+        response = requests.get("https://api.coingecko.com/api/v3/exchange_rates")
+        return response.json()
